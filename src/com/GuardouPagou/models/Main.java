d35a29988b7df476851c25a4bf38ce91a37b9f36
JP-Estudante <joaopeschool@gmail.com>
2025-06-15 18:57:18 -0300

    Remove bundled Poppins font files

--- src/com/GuardouPagou/models/Main.java
+++ src/com/GuardouPagou/models/Main.java
@@ -5,12 +5,12 @@ import com.GuardouPagou.controllers.MainController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-
         MainView mainView = new MainView();
         new MainController(mainView);
 
